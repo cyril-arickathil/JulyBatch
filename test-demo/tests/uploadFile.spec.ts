213cc8e9fa6189aedba9e35d1a133ed369d9a10f
Cyril Arickathil <cyriljohn21@gmail.com>
2025-09-12 08:21:55 +0530

    12sep

---
+++ test-demo/tests/uploadFile.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test";
+
+test('upload file', async ({page})=>
+{
+  await page.goto("https://the-internet.herokuapp.com/upload");
+
+  //<input type="file" id="fileInput" name="file">
+  await page.setInputFiles('#file-upload','tests/uploadFiles/Preview.png' )
+  //multiple files
+  await page.setInputFiles('#file-upload', ['tests/uploadFiles/Preview.png', 'tests/uploadFiles/sample-1mb.docx'] )
+})
+
+test('download file', async ({page})=>
+{
+  await page.goto("https://the-internet.herokuapp.com/download");
+  const downloadLink = page.locator('a', {hasText: 'DummyFile.txt'});
+
+  //wait for the download to start
+  const [ download ] = await Promise.all(
+    [
+      page.waitForEvent('download'), //waits for the download to start
+      downloadLink.click(), //triggers the download
+    ]
+  )
+  await download.saveAs('tests/downloadFiles/DummyFile1.txt');
+})
+
+// page.on events
+//page events
+//download event
+// new tabs
+//new windows
+
+//handle this events  ? 
+//page.waitForEvent('download')
+
+
+//see and event , before u action on that event
